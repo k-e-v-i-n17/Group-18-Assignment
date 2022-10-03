d3d2b80a83d286ad9f05b922b233f284677dcbd6
BollinaPranav <bollinap@tcd.ie>
2022-10-03 10:59:59 +0100

    This is a blank canvas for now we can implement our code in this

--- Group18Calculator.java
+++ Group18Calculator.java
@@ -1,9 +1,41 @@
-public class Group18Calculator {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class Group18Calculator implements ActionListener{
+
+	JFrame frame;
+
+	JTextField textfield;
+	JButton[] numberButtons = new JButton[10];
+	JButton[] functionButotns = new JButton[8];
+	JButton addButton,subButton, mulButton,clrButton;
+	JPanel panel;
+	
+	Font myFont = new Font("Ink Free", Font.BOLD,30);
+	
+	double num1=0, num2=0, num3=0;
+	char operator;
+
+	Group18Calculator (){
+		
+		frame = new JFrame("Calculator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,550);
+		frame.setLayout(null);
+		
+		
+		
+		frame.setVisible(true);
+	}
 	public static void main(String[] args) {
-		
-		System.out.print("This is a test :)");
-		
+		// TODO Auto-generated method stub
+
+		Group18Calculator calc = new Group18Calculator();
+	}
+
+	public void actionPerformed(ActionEvent e) {
+
 	}
 
 }
